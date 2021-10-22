ffadce6d99b480184b82a75a86de7a3f956dbc24
Joanna <m.joanna.foss@gmail.com>
2021-10-22 13:50:00 -0500

    Console IO lecture

---
+++ src/ConsoleIO.java
@@ -0,0 +1,11 @@
+public class ConsoleID {
+    public static void main(String[] args){
+        String name = "Quasar";
+        System.out.printf("Hello there, %s. Nice to see you.\n", name);
+
+        String greeting = "Howdy";
+        String cohortName = "Quasar";
+
+        System.out.printf("%s, %s!", greeting, cohortName);
+    }
+}
