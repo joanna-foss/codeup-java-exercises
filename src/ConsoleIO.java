4227a45844d779f5afe47f64f4daea07b9ab42f1
Joanna <m.joanna.foss@gmail.com>
2021-10-22 14:27:39 -0500

    Console IO lecture material

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -1,11 +1,26 @@
-public class ConsoleID {
+import java.util.Scanner;
+
+public class ConsoleIO {
     public static void main(String[] args){
-        String name = "Quasar";
-        System.out.printf("Hello there, %s. Nice to see you.\n", name);
 
-        String greeting = "Howdy";
-        String cohortName = "Quasar";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("%s, %s!", greeting, cohortName);
+//        String name = "Quasar";
+//        System.out.printf("Hello there, %s. Nice to see you.\n", name);
+//
+//        String greeting = "Howdy";
+//        String cohortName = "Quasar";
+//
+//        System.out.printf("%s, %s!", greeting, cohortName);
+
+//        System.out.print("Enter something: ");
+//
+//        String userInput = scanner.nextLine();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.printf("You entered: %s", userInput);
     }
 }
