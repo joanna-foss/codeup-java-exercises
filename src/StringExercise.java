2271f7e3df359353eada87a5b4ff236008fe2874
Joanna <m.joanna.foss@gmail.com>
2021-10-26 10:26:16 -0500

    started: string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+
+    }
+}
