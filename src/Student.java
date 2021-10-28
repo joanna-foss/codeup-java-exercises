601075ffbd662a88da553323820aa61904f95478
Joanna <m.joanna.foss@gmail.com>
2021-10-28 13:52:36 -0500

    finished: OOP and constructors lec

--- src/Student.java
+++ src/Student.java
@@ -12,10 +12,15 @@ public class Student {
         //First constructor experiment:
         Student kenneth = new Student("Kenneth H.", "Deimos");
         System.out.printf("%s is the student and %s is their cohort.", kenneth.name, kenneth.cohort);
+
+        //Second constructor experiement:
+        Student mcKenneth = new Student("mcKen mcKow", "mcDeimos", 88.8);
+        System.out.println(mcKenneth.grade);
     }
 
     public String name;
     public String cohort;
+    private double grade;
 
     //Now, let's add a constructor - what is it? A METHOD OF OBJECT CREATION
     public Student(String studentName){
@@ -26,5 +31,14 @@ public class Student {
     public Student(String studentName, String cohortName){
         this.name = studentName;
         this.cohort = cohortName;
+    }
+    public Student(String studentName, String cohortName, double grade) {
+        this.name = studentName;
+        this.cohort = cohortName;
+        this.grade = grade;
+    }
+
+    private double shareGrade() {
+        return grade;
     }
 }
