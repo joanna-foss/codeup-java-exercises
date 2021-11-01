c1fc4733571c751f89873159942ff12073652aca
Joanna <m.joanna.foss@gmail.com>
2021-11-01 09:29:03 -0500

    edits to warm up

--- src/Candy.java
+++ src/Candy.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
-
+//my version of this warmup
 public class Candy {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What candy bar are you buying?\n");
-        String candy = scanner.nextLine();
-        System.out.printf("A %s! What size are you buying?\n", candy);
-        String size = scanner.nextLine();
+    public static String candy;
+    public static String size;
 
-        Candy(candy, size);
+    public static void main(String[] args) {
+        Candy candy1 = new Candy("Twix");
+        Candy candy2 = new Candy("PayDay", "King");
+        System.out.println(candy1.displayInfo());
+        System.out.println(candy2.displayInfo());
     }
 
-    public static void Candy(String name, String size){
-        System.out.printf("Ahhh, a %s! I see that you got the %s of it. Sounds good! :)", name, size);
+    public Candy(String name){
+        candy = name;
+        size = "undecided";
+    }
+
+    public Candy(String name, String chosen){
+        candy = name;
+        size = chosen;
+    }
+
+    public String displayInfo(){
+        return String.format("Ahh, a %s! I see that you got the %s of it. Sounds good! :)", candy, size);
     }
 }
 
