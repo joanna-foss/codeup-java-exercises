3662a6fab006a48247367dc8efbfceb00f2b5276
Joanna <m.joanna.foss@gmail.com>
2021-11-01 11:45:53 -0500

    started: arrays exercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,6 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers); //this returns the object's location in memory. Must use toString to get the actual array value.
+    }
+}
