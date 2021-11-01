46e37317b2ddda270531a5dc76b77a805c410bbf
Joanna <m.joanna.foss@gmail.com>
2021-11-01 16:05:53 -0500

    completed: arrays exercises

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -10,44 +10,74 @@ public class MoviesApplication {
     public static Movie[] movies = MoviesArray.findAll();
 
     public static void main(String[] args) {
-        for(int i = 0; i < movies.length; i++){
-            System.out.println(movies[i].getName());
+        System.out.println("What would you like to do?\n");
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view animated movies");
+        System.out.println("3 - view dramatic movies");
+        System.out.println("4 - view horrific movies");
+        System.out.println("5 - view scifi movies");
+        System.out.println("6 - view comedy movies\n");
+
+        int choice = Input.getInt(0, 6);
+
+        switch(choice){
+            case 0:
+                System.out.println("Okay, later!");
+                break;
+            case 1:
+                for (int i = 0; i < movies.length; i++) {
+                    System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                }
+                break;
+            case 2:
+                for (int i = 0; i < movies.length; i++) {
+                    if(movies[i].getCategory().equalsIgnoreCase("animated")) {
+                        System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                    } else {
+                        continue;
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < movies.length; i++) {
+                    if(movies[i].getCategory().equalsIgnoreCase("drama")) {
+                        System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                    } else {
+                        continue;
+                    }
+                }
+                break;
+            case 4:
+                for (int i = 0; i < movies.length; i++) {
+                    if(movies[i].getCategory().equalsIgnoreCase("horror")) {
+                        System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                    } else {
+                        continue;
+                    }
+                }
+                break;
+            case 5:
+                for (int i = 0; i < movies.length; i++) {
+                    if(movies[i].getCategory().equalsIgnoreCase("scifi")) {
+                        System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                    } else {
+                        continue;
+                    }
+                }
+                break;
+            case 6:
+                for (int i = 0; i < movies.length; i++) {
+                    if(movies[i].getCategory().equalsIgnoreCase("comedy")) {
+                        System.out.println("Title: " + movies[i].getName() + " || Category: " + movies[i].getCategory());
+                    } else {
+                        continue;
+                    }
+                }
+                break;
+            default:
+                System.out.println("Try again later.");
         }
-
-
-//        System.out.println("What would you like to do?\n");
-//        System.out.println("0 - exit");
-//        System.out.println("1 - view all movies");
-//        System.out.println("2 - view animated movies");
-//        System.out.println("3 - view dramatic movies");
-//        System.out.println("4 - view horrific movies");
-//        System.out.println("5 - view scifi movies\n");
-//
-//        int choice = Input.getInt(0, 5);
-//
-//        switch(choice){
-//            case 0:
-//                System.out.println("Okay, later!");
-//                break;
-//            case 1:
-//                System.out.println("case 1");
-////                System.out.println(Arrays.toString(MoviesArray.findAll()));
-//                break;
-//            case 2:
-//                System.out.println("case 2");
-//                break;
-//            case 3:
-//                System.out.println("case 3");
-//                break;
-//            case 4:
-//                System.out.println("case 4");
-//                break;
-//            case 5:
-//                System.out.println("case 5");
-//                break;
-//            default:
-//                System.out.println("Try again later.");
-//        }
 
     }
 }
