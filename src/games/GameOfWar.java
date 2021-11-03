790e08b7da7c258daddbbd620f72131501a0158a
Joanna <m.joanna.foss@gmail.com>
2021-11-03 11:10:48 -0500

    completed: the game of war

--- src/games/GameOfWar.java
+++ src/games/GameOfWar.java
@@ -9,41 +9,58 @@ import java.util.Scanner;
 public class GameOfWar {
     public static int numOfRounds = 26;
     public static Card[] cards = CardsArray.findAll();
-    public static Card[] cardsCopy = {};
     public static Scanner scanner = new Scanner(System.in);
     public static boolean ready;
 
     public static void main(String[] args) {
         Player player1 = new Player();
+        Player computer = new Player();
         System.out.println("You have begun the Game of War!");
-//        for(int i = 0; i < cards.length; i++){
-//            System.out.println("cards[i].getCard() = " + cards[i].getCard());
-//        }
+
         player1.setName();
         System.out.printf("%s, you will play 26 rounds and whoever plays the higher card each round wins that round.%n", player1.name);
-        System.out.printf("%s, are you ready to begin?", player1.name);
+        System.out.println("Win more rounds than the computer and you win the game!\n");
+        System.out.printf("%s, enter 'yes' when you are ready to begin.", player1.name);
         ready = scanner.next().equalsIgnoreCase("yes");
+
         if (ready) {
             do {
                 Card computerCard = new Card(getRandomCard());
                 Card playerCard = new Card(getRandomCard());
 
-                System.out.println("computerCard = " + computerCard.getCardInfo());
-                System.out.println("playerCard = " + playerCard.getCardInfo());
-                System.out.println("Continue?");
-                ready = scanner.next().equalsIgnoreCase("yes");
+                System.out.println("\nThe computer played: " + computerCard.getCardInfo());
+                System.out.println("You played: " + playerCard.getCardInfo());
                 numOfRounds--;
+                if(computerCard.numValue > playerCard.numValue){
+                    System.out.println("\nThe computer won this round!\n");
+                    computer.addWin();
+                } else if (playerCard.numValue > computerCard.numValue){
+                    System.out.println("\nYou won this round!\n");
+                    player1.addWin();
+                } else {
+                    System.out.println("\nYou tied! No one wins this round.\n");
+                }
+                System.out.printf("So far, the computer has %s wins and you have %s wins!%n", computer.gamesWon, player1.gamesWon);
+                System.out.println("Enter anything to play the next round.");
+                scanner.next();
             } while (numOfRounds > 0);
         }
-    }
-        ;
 
-        public static Card getRandomCard() {
-            Random random = new Random();
-            int number = random.nextInt(51) + 1;
-            return cards[number];
+        if(player1.gamesWon> computer.gamesWon){
+            System.out.printf("%nCONGRATS!!! You finished the game with a total of %s wins. YOU WON THE GAME OF WAR!%n", player1.gamesWon);
+        } else if (computer.gamesWon>player1.gamesWon){
+            System.out.printf("%nGAH! The computer beat you with a total of %s wins. YOU LOST THE GAME OF WAR.%n", computer.gamesWon);
+        } else {
+            System.out.println("%nLooks like it's a tie! No defeat, but also no loss!%n");
         }
     }
+
+    public static Card getRandomCard() {
+        Random random = new Random();
+        int number = random.nextInt(51) + 1;
+        return cards[number];
+    }
+}
 
 // MY ATTEMPTS AT CREATING A NEW ARRAY AFTER EVERY CARD DRAWN. DOES NOT WORK.
 //    public Card[] getCards(){
