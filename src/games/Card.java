32ad440a38ecc98209229c7fc033b0e99a03ff0c
Joanna <m.joanna.foss@gmail.com>
2021-11-04 15:37:16 -0500

    completed: game of war

--- src/games/Card.java
+++ src/games/Card.java
@@ -1,18 +1,14 @@
 package games;
 
-import java.util.Random;
-
 public class Card {
     String suit;
     String value;
     int numValue;
 
-    public Card(){}
-
-    public Card(Card randomCard){
-        this.suit = randomCard.suit;
-        this.value = randomCard.value;
-        this.numValue = randomCard.numValue;
+    public Card(Card card){
+        this.suit = card.suit;
+        this.value = card.value;
+        this.numValue = card.numValue;
     }
 
     public Card(String cardSuit, String cardValue, int numValue){
